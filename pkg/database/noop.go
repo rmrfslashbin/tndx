4b72cd46860e8eda11f878ccd35943fc4c9183a7
Robert Sigler <sigler@improvisedscience.org>
2021-12-07 12:30:30 -0500

    Adding noop database and storage drivers

--- pkg/database/noop.go
+++ pkg/database/noop.go
@@ -2,15 +2,15 @@ package database
 
 import "github.com/sirupsen/logrus"
 
-type NoOpDBOption func(config *SqliteDatabaseDriver)
+type NoopDBOption func(config *NoopDBDriver)
 
-type NoOpDBDriver struct {
+type NoopDBDriver struct {
 	log        *logrus.Logger
 	driverName string
 }
 
-func NewNoOpDB(opts ...func(*NoOpDBDriver)) *NoOpDBDriver {
-	config := &DDBDriver{}
+func NewNoopDB(opts ...func(*NoopDBDriver)) *NoopDBDriver {
+	config := &NoopDBDriver{}
 	config.driverName = "noop"
 
 	// apply the list of options to Config
@@ -19,4 +19,58 @@ func NewNoOpDB(opts ...func(*NoOpDBDriver)) *NoOpDBDriver {
 	}
 
 	return config
+}
+
+func SetNoopDBLogger(log *logrus.Logger) NoopDBOption {
+	return func(config *NoopDBDriver) {
+		config.log = log
+	}
+}
+
+func (config *NoopDBDriver) GetDriverName() string {
+	return config.driverName
+}
+
+func (config *NoopDBDriver) GetFavoritesConfig(userID int64) (*TweetConfigQuery, error) {
+	return nil, nil
+}
+
+func (config *NoopDBDriver) GetFollowersConfig(userID int64) (*CursoredTweetConfigQuery, error) {
+	return nil, nil
+}
+
+func (config *NoopDBDriver) GetFriendsConfig(userID int64) (*CursoredTweetConfigQuery, error) {
+	return nil, nil
+}
+
+func (config *NoopDBDriver) GetTimelineConfig(userID int64) (*TweetConfigQuery, error) {
+	return nil, nil
+}
+
+func (config *NoopDBDriver) PutFavoritesConfig(query *TweetConfigQuery) error {
+	return nil
+}
+
+func (config *NoopDBDriver) PutFollowersConfig(query *CursoredTweetConfigQuery) error {
+	return nil
+}
+
+func (config *NoopDBDriver) PutFriendsConfig(query *CursoredTweetConfigQuery) error {
+	return nil
+}
+
+func (config *NoopDBDriver) PutTimelineConfig(query *TweetConfigQuery) error {
+	return nil
+}
+
+func (config *NoopDBDriver) PutRunnerFlags(runnerName string, userid int64, flags Bits) error {
+	return nil
+}
+
+func (config *NoopDBDriver) GetRunnerUsers(runner string, userID int64) ([]*RunnerFlagsItem, error) {
+	return nil, nil
+}
+
+func (config *NoopDBDriver) DeleteRunnerUser(runnerName string, userid int64) error {
+	return nil
 }
---
+++ pkg/storage/noop.go
@@ -0,0 +1,27 @@
+package storage
+
+type NoopOption func(c *LocalStorageDriver)
+
+type NoopStorageDriver struct {
+	driverName string
+}
+
+func NewNoopStorage(opts ...func(*NoopStorageDriver)) *NoopStorageDriver {
+	config := &NoopStorageDriver{}
+	config.driverName = "noop"
+
+	// apply the list of options to Config
+	for _, opt := range opts {
+		opt(config)
+	}
+
+	return config
+}
+
+func (config *NoopStorageDriver) Put(name string, body []byte) error {
+	return nil
+}
+
+func (config *NoopStorageDriver) GetDriverName() string {
+	return config.driverName
+}
