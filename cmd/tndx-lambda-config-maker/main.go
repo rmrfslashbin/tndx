f223eafb1550455d3315620589ba306c92130a70
Robert Sigler <sigler@improvisedscience.org>
2021-12-28 21:12:30 -0500

    Adding function

---
+++ cmd/tndx-lambda-config-maker/main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"os"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	AwsRegion        string `yaml:"aws_region"`
+	AwsProfile       string `yaml:"aws_profile"`
+	DDBTablePrefix   string `yaml:"ddb_table_prefix"`
+	TwitterAPIKey    string `yaml:"twitter_api_key"`
+	TwitterAPISecret string `yaml:"twitter_api_secret"`
+}
+
+type S3Request struct {
+	Bucket string `json:"bucket"`
+	Key    string `json:"key"`
+	Region string `json:"region"`
+}
+
+var (
+	aws_region string
+	log        *logrus.Logger
+)
+
+func init() {
+	log = logrus.New()
+	log.SetLevel(logrus.InfoLevel)
+	log.SetFormatter(&logrus.JSONFormatter{})
+	aws_region = os.Getenv("AWS_REGION")
+}
+
+func main() {
+	// Catch errors
+	var err error
+	defer func() {
+		if err != nil {
+			logrus.WithFields(logrus.Fields{
+				"error": err,
+			}).Fatal("main crashed")
+		}
+	}()
+	lambda.Start(handler)
+}
+
+func handler(ctx context.Context, s3Request S3Request) error {
+	cfg := Config{
+		AwsRegion:        os.Getenv("AWS_REGION"),
+		AwsProfile:       os.Getenv("AWS_PROFILE"),
+		DDBTablePrefix:   os.Getenv("DDB_TABLE_PREFIX"),
+		TwitterAPIKey:    os.Getenv("TWITTER_API_KEY"),
+		TwitterAPISecret: os.Getenv("TWITTER_API_SECRET"),
+	}
+	yml, err := yaml.Marshal(&cfg)
+	if err != nil {
+		log.WithFields(
+			logrus.Fields{
+				"error":  err,
+				"config": cfg,
+				"s3":     s3Request,
+			}).Error("failed to marshal config")
+		return err
+	}
+
+	awsconfig, err := config.LoadDefaultConfig(context.TODO(), func(o *config.LoadOptions) error {
+		o.Region = s3Request.Region
+		return nil
+	})
+	if err != nil {
+		log.WithFields(
+			logrus.Fields{
+				"error":  err,
+				"config": cfg,
+				"s3":     s3Request,
+			}).Error("failed to config S3")
+		return err
+	}
+
+	s3Svc := s3.NewFromConfig(awsconfig)
+	obj, err := s3Svc.PutObject(ctx, &s3.PutObjectInput{
+		Bucket: &s3Request.Bucket,
+		Key:    &s3Request.Key,
+		Body:   bytes.NewReader(yml),
+	})
+	if err != nil {
+		log.WithFields(
+			logrus.Fields{
+				"error":  err,
+				"config": cfg,
+				"s3":     s3Request,
+			}).Error("failed to put object")
+		return err
+	}
+	log.WithFields(logrus.Fields{
+		"config":       cfg,
+		"s3":           s3Request,
+		"outputObject": obj,
+	}).Info("successfully wrote config")
+	return nil
+}
--- go.mod
+++ go.mod
@@ -13,17 +13,18 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/rekognition v1.13.0
 	github.com/aws/aws-sdk-go-v2/service/sqs v1.13.1
 	github.com/aws/aws-sdk-go-v2/service/ssm v1.17.1
-	github.com/davecgh/go-spew v1.1.1
 	github.com/dghubble/go-twitter v0.0.0-20211115160449-93a8679adecb
 	github.com/rmrfslashbin/ssmparams v1.0.0
 	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/cobra v1.2.1
 	github.com/spf13/viper v1.9.0
 	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 	modernc.org/sqlite v1.14.2
 )
 
 require (
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.0.0 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.6.4 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.8.2 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.2 // indirect
@@ -33,6 +34,8 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.5.0 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery v1.3.3 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.5.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.9.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.22.0 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sso v1.6.2 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.11.1 // indirect
 	github.com/aws/smithy-go v1.9.0 // indirect
